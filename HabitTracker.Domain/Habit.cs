f54393f93a176b9fa67929c23a529723fb5aed72
FiJlv <vladzzzzxd@gmail.com>
2022-05-21 12:14:16 +0300

    Architecture

---
+++ HabitTracker.Domain/Habit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HabitTracker.Domain
+{
+    public class Habit
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Instruction { get; set; }
+        public string HabitDays { get; set; }
+        public DateTime CreationDate { get; set; }
+
+    }
+}
